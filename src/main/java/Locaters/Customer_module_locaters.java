0c19cabe1879e1d71b95115a31e93b2325e2a605
AyanAutomation <sengupta.ayan90@gmail.com>
2025-09-07 19:29:00 +0530

    Customer list data collector addded

--- src/main/java/Locaters/Customer_module_locaters.java
+++ src/main/java/Locaters/Customer_module_locaters.java
@@ -1,5 +1,8 @@
 package Locaters;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -12,9 +15,9 @@ public class Customer_module_locaters extends Generic_codes{
 	
 	
 	@FindBy(xpath="//*[text()='Admin | Customer']")
-	private WebElement  Landed_on_customer_list; /*
-	@FindBy(xpath="")
-	private WebElement  ;
+	private WebElement  Landed_on_customer_list; 
+	@FindBy(xpath="//tbody")
+	private WebElement customer_list; /*
 	@FindBy(xpath="")
 	private WebElement  ;
 	@FindBy(xpath="")
@@ -36,22 +39,30 @@ public class Customer_module_locaters extends Generic_codes{
 	
     public WebElement Landed_on_customer_list(){
     WebElementWait(Landed_on_customer_list);
-    return Landed_on_customer_list;}  /*
-    public WebElement (){
-    WebElementWait();
-    return ;} 
-    public WebElement (){
-    WebElementWait();
-    return ;} 
-    public WebElement (){
-    WebElementWait();
-    return ;} 
-    public WebElement (){
-    WebElementWait();
-    return ;} 
-    public WebElement (){
-    WebElementWait();
-    return ;} 
+    return Landed_on_customer_list;}  
+    public WebElement customer_list(){
+    WebElementWait(customer_list);
+    return customer_list;} 
+    public List<WebElement> names(){
+    customer_list();
+    List<WebElement> names = customer_list().findElements(By.xpath(".//td[3]"));
+    WebElementWait(names);
+    return names;} 
+    public List<WebElement> Customer_phnums(){
+    customer_list();
+    List<WebElement> phnums = customer_list().findElements(By.xpath(".//td[5]"));
+    WebElementWait(phnums);
+    return phnums;} 
+    public List<WebElement> Customer_email(){
+    customer_list();
+    List<WebElement> email = customer_list().findElements(By.xpath(".//td[4]"));
+    WebElementWait(email);
+    return email;}
+    public List<WebElement> Customer_Status(){
+    customer_list();
+    List<WebElement> stats = customer_list().findElements(By.xpath(".//td[6]"));
+    WebElementWait(stats);
+    return stats;} /*
     public WebElement (){
     WebElementWait();
     return ;} 
--- src/main/java/Whippets/com/Admin/Customer_Module.java
+++ src/main/java/Whippets/com/Admin/Customer_Module.java
@@ -1,7 +1,12 @@
 package Whippets.com.Admin;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.stream.IntStream;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 import Locaters.Customer_module_locaters;
@@ -9,6 +14,14 @@ import Locaters.Product_Module_locaters;
 
 public class Customer_Module extends Product_module{
 	
+	
+	TreeMap<String, String> customerNames_phnumbers = new TreeMap<String, String>();
+	TreeMap<String, String> customerNames_Emails = new TreeMap<String, String>();
+	TreeMap<String, String> customerNames_Statuses = new TreeMap<String, String>();
+	List <String> customers_names = new ArrayList<String>();
+	List <String> customers_phones = new ArrayList<String>();
+	List <String> customers_Emails= new ArrayList<String>();
+	List <String> customers_Statuses= new ArrayList<String>();
 	
 	@Test
 	public void customer_list_Accessor() throws IOException, InterruptedException{
@@ -21,7 +34,48 @@ public class Customer_Module extends Product_module{
 		po.select_dropdown();
 		Select s = new Select(po.select_dropdown());
 		s.selectByVisibleText("100");
-		Thread.sleep(800);
+		Thread.sleep(800);}
+	
+	
+	
+	@Test
+	public void customer_List_Data_Collector() throws IOException, InterruptedException{
+		
+		Customer_module_locaters p = new Customer_module_locaters(d);
+		Product_Module_locaters po = new Product_Module_locaters(d);
+		
+		customer_list_Accessor();
+		p.customer_list();
+		allList_clear();
+		List <WebElement> customers_name_element = p.names();
+		List <WebElement> customers_phones_element = p.Customer_phnums();
+		List <WebElement> customers_Emails_element = p.Customer_email();
+		List <WebElement> customers_Statuses_element = p.Customer_Status();
+		IntStream.range(0, customers_name_element.size()).forEach(i->{
+			customers_names.add(customers_name_element.get(i).getText());
+			customers_phones.add(customers_phones_element.get(i).getText());
+			customers_Emails.add(customers_Emails_element.get(i).getText());
+			customers_Statuses.add(customers_Statuses_element.get(i).getText());
+			});
+		if(customers_names.size()==customers_phones.size()&&customers_Emails.size()==customers_Statuses.size()&&customers_Emails.size()==customers_names.size()){
+			IntStream.range(0,customers_names.size()).forEach(k->{
+				customerNames_phnumbers.put(customers_names.get(k), customers_phones.get(k));
+				customerNames_Emails.put(customers_names.get(k), customers_Emails.get(k));
+				customerNames_Statuses.put(customers_names.get(k), customerNames_Statuses.get(k));});}}
+	
+	
+	
+	public void allList_clear(){
+		
+		customers_names.clear();
+		customers_phones.clear();
+		customers_Emails.clear();
+		customers_Statuses.clear();
+		customerNames_phnumbers.clear();
+		customerNames_Emails.clear();
+		customerNames_Statuses.clear();
+		
+		
 		
 	}
 
